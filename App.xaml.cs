caf035190e678e078a6e4e4e4edc819f71ddd196
b2 <WSR@WSR-PC>
2024-10-28 13:47:11 +0300

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using ForksnSpoons.database;
+namespace ForksnSpoons
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static User user;
+        public static bool login(String login, String password)
+        {
+            User user = Database.context.User.Where(u => u.Login == login & u.Password == password).First();
+            if (user != null)
+            {
+                App.user = user;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ database/Database.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForksnSpoons.database
+{
+    class Database 
+    {
+        public static ForksnSpoonsEntities context = new ForksnSpoonsEntities();
+    }
+}
